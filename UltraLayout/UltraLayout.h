691a4d4e9615c1968628577b309e7feb13baf1cb
Brent Royal-Gordon <brent@architechies.com>
2014-04-06 16:12:35 -0700

    Initial Commit

---
+++ UltraLayout/UltraLayout.h
@@ -0,0 +1,13 @@
+//
+//  UltraLayout.h
+//  UltraLayout
+//
+//  Created by Brent Royal-Gordon on 4/6/14.
+//  Copyright (c) 2014 Architechies. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UltraLayout : NSObject
+
+@end
---
+++ UltraLayout/UltraLayout.m
@@ -0,0 +1,13 @@
+//
+//  UltraLayout.m
+//  UltraLayout
+//
+//  Created by Brent Royal-Gordon on 4/6/14.
+//  Copyright (c) 2014 Architechies. All rights reserved.
+//
+
+#import "UltraLayout.h"
+
+@implementation UltraLayout
+
+@end
